77bc6de192eb63f7649d911a74ba58b1f09aff46
Mohd Sami <94985765+MohdSami234@users.noreply.github.com>
2021-12-29 23:49:02 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,11 @@
+// Assignment-1 --
+// Q1. Write a program to check whether the number is greater than 10 or not
+var a = prompt("Enter the no.")
+a = parseInt(a);
+if(a>10){
+alert("Number is greater than 10")
+}
+else{
+alert("Number is not greater than 10")
+}
+
